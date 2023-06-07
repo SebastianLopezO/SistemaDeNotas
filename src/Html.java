257b0ec99df88230491d9dd329f21baefb03b992
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2023-06-07 18:40:45 -0500

    Update Html.java

--- src/Html.java
+++ src/Html.java
@@ -10,23 +10,23 @@ public class Html {
 
     public Html() {
         //Head Estandar
-        this.Head = "<head>
-                        <title>Tabla de Estudiantes</title>
-                        <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'>
-                    </head>";
-        this.Body = '<h1>Tabla de Estudiantes</h1>
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">ID</th>
-                                <th scope="col">Nombre</th>
-                                <th scope="col">Apellido</th>
-                                <th scope="col">Materias</th>
-                                <th scope="col">Carrera</th>
-                                <th scope="col">Promedio</th>
-                            </tr>
-                        </thead>
-                        <tbody>';
+        this.Head = "<head>" +
+                        "<title>Tabla de Estudiantes</title>" +
+                        "<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'>" +
+                    "</head>";
+        this.Body = "<h1>Tabla de Estudiantes</h1>" +
+                    "<table class='table'>" +
+                    "<thead>" +
+                        "<tr>" +
+                            "<th scope='col'>ID</th>" +
+                            "<th scope='col'>Nombre</th>" +
+                            "<th scope='col'>Apellido</th>" +
+                            "<th scope='col'>Materias</th>" +
+                            "<th scope='col'>Carrera</th>" +
+                            "<th scope='col'>Promedio</th>" +
+                        "</tr>" +
+                    "</thead>" +
+                    "<tbody>";
     }
 
     public void AddBody(String html) {
@@ -42,7 +42,7 @@ public class Html {
             // Escribir código HTML en archivo
             FileWriter fw = new FileWriter(archivo);
             BufferedWriter bw = new BufferedWriter(fw);
-            bw.write('<html>'+this.Head +'<body><div class="container">'+ this.Body+'</tbody></div></body>'+'<html>');
+            bw.write("<html>" + this.Head + "<body><div class='container'>" + this.Body + "</tbody></div></body>" + "<html>");
             bw.close();
 
             // Abrir el archivo HTML en el navegador predeterminado
