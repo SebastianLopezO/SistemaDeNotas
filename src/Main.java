edd607d50e75094c4237f27356f22e8124f788d2
Sebastián López Osorno <100486485+SebastianLopezO@users.noreply.github.com>
2023-06-07 12:39:47 -0500

    main

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         ListaEstudiantes Lista=new ListaEstudiantes("PJIC");
         Lista.Insert("01","Sebastian","Lopez","Ing Informatica");
-        (Lista.getPunta()).Insert("Ingles");
+        ((Lista.getPunta()).getTope()).Insert(5);
         Lista.Show();
     }
 }
--- src/Materia.java
+++ src/Materia.java
@@ -39,4 +39,26 @@ public class Materia {
     public void setVec(int[] vec) {
         this.vec = vec;
     }
+
+    public boolean ColaLlena(){
+        if(this.Tope==this.Lim){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean ColaVacia()){
+        if(this.Tope==-1){
+            return true;
+        }
+        return false;
+    }
+
+    public void Insert(int nota){
+        if(!ColaLlena()){
+            Tope++;
+            Vec[Tope]==nota;
+        }
+        System.out.println("Ya no se puede ingresar mas notas");
+    }
 }
