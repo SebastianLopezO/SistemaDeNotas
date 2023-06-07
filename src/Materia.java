7a2fc7bc99e3dd2016971d48da3d993f1f1844fa
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2023-06-06 20:59:44 -0500

    main

--- src/Materia.java
+++ src/Materia.java
@@ -1,4 +1,42 @@
 public class Materia {
-    private String Nombre,Codigo;
-    private PilaNotas Notas;
+    private String Nombre;
+    private int Tope,Lim;
+    private int[] vec;
+
+    public Materia(String Nombre){
+        this.Nombre=Nombre;
+        this.Tope=-1;
+        this.Lim=4;
+    }
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public int getTope() {
+        return Tope;
+    }
+
+    public void setTope(int tope) {
+        Tope = tope;
+    }
+
+    public int getLim() {
+        return Lim;
+    }
+
+    public void setLim(int lim) {
+        Lim = lim;
+    }
+
+    public int[] getVec() {
+        return vec;
+    }
+
+    public void setVec(int[] vec) {
+        this.vec = vec;
+    }
 }
