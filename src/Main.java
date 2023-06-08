4f1ac09995a0b504e66f2c5eb05eff47a968b8f4
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2023-06-07 20:50:54 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,49 @@
 import javax.swing.*;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        ListaEstudiantes Lista=new ListaEstudiantes("PJIC");
-        Lista.Insert("01","Sebastian","Lopez","Ing Informatica");
+        ListaEstudiantes Lista = new ListaEstudiantes("PJIC");
+        boolean app = true;
+        while (app) {
+            String Option = Menu();
+            switch (Option) {
+                case "Insertar Estudiante":
+                    break;
+                case "Buscar Estudiante":
+                    String doc = Integer.toString(GetId(Option));
+                    Estudiante X = Lista.Buscar(doc);
+                    boolean est = true;
+                    if (X == null) {
+                        System.out.println("Este estudiante con cedula " + doc + " no existe");
+                        est = false;
+                    }
+                    while (est) {
+                        String OptionEst = MenuEst(X.getNombre() + " " + X.getApellido(), X.getId());
+                        switch (OptionEst) {
+                            case "Agregar Materia":
+                                break;
+                            case "Buscar Materia":
+                                break;
+                            case "Vaciar Materias":
+                                break;
+                            case "Modificar Estudiante":
+                                break;
+                            case "Eliminar Estudiante":
+                                break;
+                            case "Volver":
+                                est=false;
+                                break;
+                        }
+                    }
+                    break;
+                case "Vaciar Lista":
+                    break;
+                case "Salir":
+                    app = false;
+                    break;
+            }
+        }
+        Lista.Insert("01", "Sebastian", "Lopez", "Ing Informatica");
         (Lista.getPunta()).Insert("Matematicas");
         ((Lista.getPunta()).getPunta()).Apilar(5);
         ((Lista.getPunta()).getPunta()).Apilar(4);
@@ -38,7 +77,7 @@ public class Main {
         return Option;
     }
 
-    public static String MenuEst(String Nombre,String ID) {
+    public static String MenuEst(String Nombre, String ID) {
         String[] Options = {"Agregar Materia",
                 "Buscar Materia",
                 "Vaciar Materias",
@@ -49,7 +88,7 @@ public class Main {
 
         String Option = (String) JOptionPane.showInputDialog(
                 null,
-                "Seleccione la accion para el Estudiante "+Nombre+"("+ID+") : ",
+                "Seleccione la accion para el Estudiante " + Nombre + "(" + ID + ") : ",
                 "Opciones",
                 JOptionPane.QUESTION_MESSAGE,
                 null,
@@ -58,7 +97,7 @@ public class Main {
         return Option;
     }
 
-    public static String MenuMateria(String Nombre,String ID, String Materia) {
+    public static String MenuMateria(String Nombre, String ID, String Materia) {
         String[] Options = {"Agregar Nota",
                 "Buscar Nota",
                 "Vaciar Notas",
@@ -69,7 +108,7 @@ public class Main {
 
         String Option = (String) JOptionPane.showInputDialog(
                 null,
-                "Seleccione la accion para el Estudiante "+Nombre+"("+ID+") en la materia de "+Materia+" : ",
+                "Seleccione la accion para el Estudiante " + Nombre + "(" + ID + ") en la materia de " + Materia + " : ",
                 "Opciones",
                 JOptionPane.QUESTION_MESSAGE,
                 null,
@@ -78,20 +117,46 @@ public class Main {
         return Option;
     }
 
-    public static String MenuNota(String Nombre,String ID, String Materia,int Nota) {
+    public static String MenuNota(String Nombre, String ID, String Materia, int Nota) {
         String[] Options = {"Modificar Nota",
-                            "Eliminar Nota",
-                            "Volver"
+                "Eliminar Nota",
+                "Volver"
         };
 
         String Option = (String) JOptionPane.showInputDialog(
                 null,
-                "Seleccione la accion para el Estudiante "+Nombre+"("+ID+") en la materia de "+Materia+" : ",
-                "Opciones para la Nota "+Nota,
+                "Seleccione la accion para el Estudiante " + Nombre + "(" + ID + ") en la materia de " + Materia + " : ",
+                "Opciones para la Nota " + Nota,
                 JOptionPane.QUESTION_MESSAGE,
                 null,
                 Options,
                 Options[0]);
         return Option;
+    }
+
+    public static int GetNum(String Option) {
+        int num;
+        while (true) {
+            try {
+                num = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el Numero a "+Option+ ": "));
+                return num;
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "No se ha insertado un numero.");
+                System.out.println("No ha insertado un numero, error" + ex);
+            }
+        }
+    }
+
+    public static int GetId(String Option) {
+        int num;
+        while (true) {
+            try {
+                num = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cedula a "+Option+ ": "));
+                return num;
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "No se ha insertado un numero.");
+                System.out.println("No ha insertado un numero, error" + ex);
+            }
+        }
     }
 }
