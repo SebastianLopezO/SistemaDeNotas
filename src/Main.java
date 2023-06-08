9ef28414d384a0553771084fd6de8ecd51287887
Sebastián López Osorno <sebastianlopezosorno2005@gmail.com>
2023-06-07 19:24:50 -0500

    main

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         ListaEstudiantes Lista=new ListaEstudiantes("PJIC");
-        Lista.Insert("01","Sebastian","Lopez","Ing Informatica");
-        ((Lista.getPunta()).getPunta()).Apilar(5);
+
         Lista.ShowHtml();
     }
 
